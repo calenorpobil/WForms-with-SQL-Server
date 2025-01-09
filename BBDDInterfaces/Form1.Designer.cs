b070b4c80e8a758abcc7c169bae0f7f317e28b49
Marco Postman <89901767+calenorpobil@users.noreply.github.com>
2025-01-09 22:11:37 +0100

    First commit

---
+++ BBDDInterfaces/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace BBDDInterfaces
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btAñadir = new System.Windows.Forms.Button();
+            this.btModificar = new System.Windows.Forms.Button();
+            this.btBorrar = new System.Windows.Forms.Button();
+            this.tbTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(26, 294);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(722, 144);
+            this.dataGridView.TabIndex = 1;
+            // 
+            // btAñadir
+            // 
+            this.btAñadir.Location = new System.Drawing.Point(589, 34);
+            this.btAñadir.Name = "btAñadir";
+            this.btAñadir.Size = new System.Drawing.Size(122, 47);
+            this.btAñadir.TabIndex = 2;
+            this.btAñadir.Text = "Añadir";
+            this.btAñadir.UseVisualStyleBackColor = true;
+            this.btAñadir.Click += new System.EventHandler(this.btAñadir_Click);
+            // 
+            // btModificar
+            // 
+            this.btModificar.Location = new System.Drawing.Point(589, 101);
+            this.btModificar.Name = "btModificar";
+            this.btModificar.Size = new System.Drawing.Size(122, 47);
+            this.btModificar.TabIndex = 3;
+            this.btModificar.Text = "Modificar";
+            this.btModificar.UseVisualStyleBackColor = true;
+            // 
+            // btBorrar
+            // 
+            this.btBorrar.Location = new System.Drawing.Point(589, 167);
+            this.btBorrar.Name = "btBorrar";
+            this.btBorrar.Size = new System.Drawing.Size(122, 47);
+            this.btBorrar.TabIndex = 4;
+            this.btBorrar.Text = "Borrar";
+            this.btBorrar.UseVisualStyleBackColor = true;
+            // 
+            // tbTextBox
+            // 
+            this.tbTextBox.Location = new System.Drawing.Point(213, 34);
+            this.tbTextBox.Name = "tbTextBox";
+            this.tbTextBox.Size = new System.Drawing.Size(296, 22);
+            this.tbTextBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbTextBox);
+            this.Controls.Add(this.btBorrar);
+            this.Controls.Add(this.btModificar);
+            this.Controls.Add(this.btAñadir);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btAñadir;
+        private System.Windows.Forms.Button btModificar;
+        private System.Windows.Forms.Button btBorrar;
+        private System.Windows.Forms.TextBox tbTextBox;
+    }
+}
+
---
+++ BBDDInterfaces/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BBDDInterfaces
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //private string connectionString = "Server=localhost;Database=TestDB;Trusted_Connection=True;";
+        string connectionString = "Data Source =DESKTOP-NNKTF0L\\SQLEXPRESS;Initial Catalog=BBDD9Eval;Integrated Security = True";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cargar();
+
+        }
+
+        private void cargar()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string selectQuery = "SELECT * FROM album";
+
+                using (var command = new SqlCommand(selectQuery, connection))
+                {
+                    using (var adapter = new SqlDataAdapter(command))
+                    {
+                        var dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+
+                        var dataGridView = this.Controls["dataGridView"] as DataGridView;
+                        dataGridView.DataSource = dataTable;
+                    }
+                }
+            }
+        }
+
+        private void btAñadir_Click(object sender, EventArgs e)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string insertQuery = "INSERT INTO album (id_album, NOMBRE, " +
+                    "num_pistas, fecha_salida, artista_creador, " +
+                    "discografica_propietaria) VALUES (@id_album, @NOMBRE, " +
+                    "@num_pistas, @fecha_salida, @artista_creador, " +
+                    "@discografica_propietaria)";
+
+                using (var command = new SqlCommand(insertQuery, connection))
+                {
+                    DateTime fecha_salida = new DateTime(2023, 1, 1);
+                    String input = "";
+                    input = tbTextBox.Text;
+                    int id_album = int.Parse(input);
+                    command.Parameters.AddWithValue("@id_album", input);
+                    command.Parameters.AddWithValue("@NOMBRE", "Juan");
+                    command.Parameters.AddWithValue("@fecha_salida", fecha_salida);
+                    command.Parameters.AddWithValue("@num_pistas", 50000);
+                    command.Parameters.AddWithValue("@artista_creador", 1);
+                    command.Parameters.AddWithValue("@discografica_propietaria", 3);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            cargar();
+        }
+    }
+}
